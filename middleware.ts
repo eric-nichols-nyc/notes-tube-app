480681aec0ad5fd2660939c502d96d9a1f0d6b0e
Eric Nichols <ebn646@hotmail.com>
2024-07-09 20:36:01 -0400

    initial commit with clerk auth

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/sso-callback(.*)",
+  "/collections(.*)",
+  "/product(.*)",
+  "/products(.*)",
+  "/product(.*)",
+  "/preview(.*)",
+  "/stores(.*)",
+  "/store(.*)",
+  "/build-a-board(.*)",
+  "/email-preferences(.*)",
+  "/blog(.*)",
+  "/about(.*)",
+  "/contact(.*)",
+  "/terms(.*)",
+  "/privacy(.*)",
+  "/api(.*)"
+]);
+
+export default clerkMiddleware((auth, request) => { 
+  // Public routes are routes that don't require authentication
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
