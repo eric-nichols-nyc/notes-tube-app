256d4c7f6c518413e0e2319ddc3844de0b050393
Eric Nichols <ebn646@hotmail.com>
2024-07-15 10:45:02 -0400

    adjusted logiin status functionality for landing page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -25,19 +25,37 @@ export default function SignInPage() {
   }
 
   return (
-    <div className="grid w-full grow items-center px-4 sm:justify-center">
+
+    
+
+<section className="bg-white">
+  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+    <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+      <img
+        alt=""
+        src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        className="absolute inset-0 h-full w-full object-cover"
+      />
+    </aside>
+
+    <main
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+    >
+      <div className="max-w-xl lg:max-w-3xl">
+        <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+          Welcome to NotesTube
+        </h1>
+
+        <p className="mt-4 leading-relaxed text-gray-500">
+        Welcome back! Please sign in to continue.
+        </p>
+      <div className="grid w-full grow items-center px-4 sm:justify-center">
       <SignIn.Root>
         <Clerk.Loading>
           {(isGlobalLoading) => (
             <>
               <SignIn.Step name="start">
                 <Card className="w-full sm:w-96">
-                  <CardHeader>
-                    <CardTitle>Sign in to NotesTube</CardTitle>
-                    <CardDescription>
-                      Welcome back! Please sign in to continue
-                    </CardDescription>
-                  </CardHeader>
                   <CardContent className="grid gap-y-4">
                     <div className="grid grid-cols-2 gap-x-4">
                       <Clerk.Connection name="github" asChild>
@@ -251,7 +269,11 @@ export default function SignInPage() {
             </>
           )}
         </Clerk.Loading>
-      </SignIn.Root>
+      </SignIn.Root> 
     </div>
+      </div>
+    </main>
+  </div>
+</section>
   );
 }
